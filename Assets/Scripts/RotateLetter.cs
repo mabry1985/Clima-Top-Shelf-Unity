11036b4b594682677241eddfd5e80d1b368b3c8d
Josh Mabry <mabry1985@gmail.com>
2020-04-20 23:16:16 -0700

    add logo animation scripts

---
+++ Assets/Scripts/RotateLetter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotateLetter : MonoBehaviour
+{
+    public new Transform transform;
+    public float speed = 1;
+
+    void Update()
+    {
+        transform.Rotate(speed, 0, 0);
+    }
+}
