3cec29b8e28eb9b290e74fb0c688ce739a3ca3e6
Josh Mabry <mabry1985@gmail.com>
2020-04-20 22:31:33 -0700

    add clouds and movement script

---
+++ Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    private Vector3 normalizeDirection;
+
+    public Transform target;
+    public float speed = 1f;
+
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
